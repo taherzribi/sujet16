e4820589be55c82ec38b8248356bac851bcb7dab
taherzribi <47568219+taherzribi@users.noreply.github.com>
2020-03-01 19:22:42 +0100

    Add files via upload

---
+++ help.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+yad --center --width=600 --height=100 --text="`echo "◦ l’option -h pour vous aider
+◦ l’option -l pour lister les utilsateurs 
+◦ l’option -p pour changer le mot de passe 
+◦ l’option -decrypt pour decrypter le mots passe
+
+◦ l’option -s pour sauvagarder le mots passe decrypter
+◦ l’option -h ou –help pour afficher le help" `"
---
+++ mdps.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+    echo " option -p"
+sudo passwd $2
+
